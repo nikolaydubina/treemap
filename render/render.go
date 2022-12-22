6f2852a9a6033b747a96a0c0fcb67590fe02f6ea
Martin Lange <44003176+mlange-42@users.noreply.github.com>
2022-12-22 12:47:42 +0100

    Slashes through entities, simplified (#27)
    * replace entity for slash by slash when contructing UIText
    
    * add test csv and corresponding svg for slashes via entities
    
    * Update render/render.go
    
    Co-authored-by: Nikolay Dubina <nikolay.dubina.pub@gmail.com>

--- render/render.go
+++ render/render.go
@@ -2,6 +2,7 @@ package render
 
 import (
 	"image/color"
+	"strings"
 	"unicode/utf8"
 
 	"github.com/nikolaydubina/treemap"
@@ -15,6 +16,11 @@ const (
 	tooSmallBoxHeight    float64 = 5
 	tooSmallBoxWidth     float64 = 5
 	textMarginH          float64 = 2
+)
+
+// entityToSlash has HTML entities to strings mapping
+var entityToSlash = strings.NewReplacer(
+	"&sol;", "/",
 )
 
 // UIText is spec on how to render text.
@@ -89,7 +95,7 @@ func (s UITreeMapBuilder) NewUIBox(node string, tree treemap.Tree, x, y, w, h, m
 	}
 
 	var textHeight float64
-	if title := tree.Nodes[node].Name; title != "" && title != "some-secret-string" {
+	if title := entityToSlash.Replace(tree.Nodes[node].Name); title != "" && title != "some-secret-string" {
 		// fit text
 		// margin here and padding to account for children
 		w := t.W - (2 * padding) - (2 * margin)
